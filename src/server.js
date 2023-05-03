9a36ff5fc41b9c790b5298f51dcad3fa524a1513
wuson <wsq815@foxmail.com>
2023-05-03 16:02:24 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+// server.js (irrelevant code omitted)
+import express from 'express'
+// Vue's server-rendering API is exposed under `vue/server-renderer`.
+import { renderToString } from 'vue/server-renderer'
+import { createApp } from './app.js'
+
+const server = express()
+
+server.get('/', (req, res) => {
+  const app = createApp()
+
+  renderToString(app).then(html => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Vue SSR Example</title>
+            <script type="importmap">
+                {
+                    "imports": {
+                        "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+                    }
+                }
+            </script>
+            <script type="module" src="/src/client.js"></script>
+        </head>
+        <body>
+            <div id="app">${html}</div>
+        </body>
+        </html>
+    `)
+  })
+})
+
+server.use(express.static('.'));
+
+server.listen(3000, () => {
+    console.log('ready')
+  })
